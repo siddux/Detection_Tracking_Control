252eab5622b7351c3a1feef2e7193b1cbc58a08b
EduardR <edu.8397.9@gmail.com>
2021-01-06 11:06:35 +0100

    Initial release

---
+++ camshift.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Eduard  Rius Martínez on 27/11/2020.
+//
+
+#include <iostream>
+#include <filesystem>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+using namespace cv;
+using namespace std;
+int main(int argc, char **argv)
+{
+
+    string filename = std::filesystem::current_path().parent_path().string() + "/slow_traffic_small.mp4";
+
+    VideoCapture capture(filename);
+    if (!capture.isOpened()){
+        //error in opening the video input
+        cerr << "Unable to open file!" << endl;
+        return 0;
+    }
+    Mat frame, roi, hsv_roi, mask;
+    // take first frame of the video
+    capture >> frame;
+    // setup initial location of window
+    Rect track_window(300, 200, 100, 50); // simply hardcoded the values
+    // set up the ROI for tracking
+    roi = frame(track_window); //Provided by YOLOm
+
+    cvtColor(roi, hsv_roi, COLOR_BGR2HSV);
+    inRange(hsv_roi, Scalar(0, 60, 32), Scalar(180, 255, 255), mask); //Threshold
+    float range_[] = {0, 180};
+    const float* range[] = {range_};
+    Mat roi_hist;
+    int histSize[] = {180};
+    int channels[] = {0};
+    calcHist(&hsv_roi, 1, channels, mask, roi_hist, 1, histSize, range);
+    normalize(roi_hist, roi_hist, 0, 255, NORM_MINMAX);
+    // Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+    TermCriteria term_crit(TermCriteria::EPS | TermCriteria::COUNT, 10, 1);
+    while(true){
+        Mat hsv, dst;
+        capture >> frame;
+        if (frame.empty())
+            break;
+        cvtColor(frame, hsv, COLOR_BGR2HSV);
+        calcBackProject(&hsv, 1, channels, roi_hist, dst, range);
+        // apply camshift to get the new location
+        RotatedRect rot_rect = CamShift(dst, track_window, term_crit);
+        // Draw it on image
+        Point2f points[4];
+        rot_rect.points(points);
+        for (int i = 0; i < 4; i++)
+            line(frame, points[i], points[(i+1)%4], 255, 2);
+        imshow("img2", frame);
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+}
+
---
+++ cmake-build-debug/CMakeFiles/detecion_tracking_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detecion_tracking_control.dir/yolo.cpp.o"
+  "detecion_tracking_control"
+  "detecion_tracking_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detecion_tracking_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
